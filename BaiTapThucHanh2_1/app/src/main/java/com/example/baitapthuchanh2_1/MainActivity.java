d2257c1625061fb00211049956fb235cf56821f6
NguyenNhatKhanhDuy <duy.nnk.65cntt@ntu.edu.vn>
2026-03-15 18:44:17 +0700

    bai thuc hanh 2.1

--- BaiTapThucHanh2_1/app/src/main/java/com/example/baitapthuchanh2_1/MainActivity.java
+++ BaiTapThucHanh2_1/app/src/main/java/com/example/baitapthuchanh2_1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.baitapthuchanh2_1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void Xulycong(View view){
+            //tim
+          EditText editTextSoA =  findViewById(R.id.edtA);
+          EditText   editTextSoB =  findViewById(R.id.edtB);
+          EditText   editTextKetQua =  findViewById(R.id.edtKQ);
+
+          //lay du lieu
+            String strA =  editTextSoA.getText().toString();
+            String strB =  editTextSoB.getText().toString();
+
+            //chuyen du lieu
+            int so_A = Integer.parseInt(strA);
+            int so_B = Integer.parseInt(strB);
+
+            // tinh toan theo yeu caau
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        // hien ra amann hinh
+        editTextKetQua.setText(strTong);
+
+
     }
 }
