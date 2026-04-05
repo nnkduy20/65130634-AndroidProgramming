78fc3fd2d0e42ae65826e4903972be597d207d10
NguyenNhatKhanhDuy <duy.nnk.65cntt@ntu.edu.vn>
2026-04-05 17:18:41 +0700

    BaiTH7_ListView1

--- BaiTH7_ListView1/app/src/main/java/com/example/baith7_listview1/MainActivity.java
+++ BaiTH7_ListView1/app/src/main/java/com/example/baith7_listview1/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.baith7_listview1;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //hhien thi du lieu
+        //b1: can du lieu
+        ArrayList<String> dsTenTinhThanhVN;
+        dsTenTinhThanhVN = new ArrayList<String>();
+        //them du lieu
+        dsTenTinhThanhVN.add("Hà Nội");
+        dsTenTinhThanhVN.add("Thành phố Hồ Chí Minh");
+        dsTenTinhThanhVN.add("Đồng Nai");
+        dsTenTinhThanhVN.add("Bình Thuận");
+        dsTenTinhThanhVN.add("Nha Trang");
+        //b2
+        ArrayAdapter<String> adapterTinhThanh;
+        adapterTinhThanh = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                                                                 dsTenTinhThanhVN
+                                                                );
+        //b3
+        ListView lvTenTinhThanh = findViewById(R.id.lvDanhSachTT);
+        lvTenTinhThanh.setAdapter(adapterTinhThanh);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
